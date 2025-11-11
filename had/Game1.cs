b934e2c89a399d27fa3b62f58af2d347473715a4
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2025-11-11 11:35:10 +0100

    komit 2

--- had/Game1.cs
+++ had/Game1.cs
@@ -8,13 +8,17 @@ namespace had
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _pixel;
+        private ctverec _ctverec;
 
+
+        private int gridSize = 80;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
            
-            _graphics.PreferredBackBufferWidth = 1000;
-            _graphics.PreferredBackBufferHeight = 1000;
+            _graphics.PreferredBackBufferWidth = 960;
+            _graphics.PreferredBackBufferHeight = 960;
             _graphics.ApplyChanges();
 
             Content.RootDirectory = "Content";
@@ -32,7 +36,13 @@ namespace had
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+
             // TODO: use this.Content to load your game content here
+            _pixel = new Texture2D(GraphicsDevice, 1, 1);
+            _pixel.SetData(new[] { Color.DarkGreen });
+
+
+            _ctverec = new ctverec(GraphicsDevice, new Vector2(80, 80), gridSize, Color.Red);
         }
 
         protected override void Update(GameTime gameTime)
@@ -52,6 +62,30 @@ namespace had
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+
+            _spriteBatch.Begin();
+          
+            DrawGrid();
+            _ctverec.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+        }
+        private void DrawGrid()
+        {
+            int width = _graphics.PreferredBackBufferWidth;
+            int height = _graphics.PreferredBackBufferHeight;
+
+            // vertikální čáry
+            for (int x = 0; x <= width; x += gridSize)
+            {
+                _spriteBatch.Draw(_pixel, new Rectangle(x, 0, 3, height), Color.DarkGreen);
+            }
+
+            // horizontální čáry
+            for (int y = 0; y <= height; y += gridSize)
+            {
+                _spriteBatch.Draw(_pixel, new Rectangle(0, y, width, 3), Color.DarkGreen);
+            }
         }
     }
 }
---
+++ had/ctverce.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace had
+{
+    public class ctverec
+    {
+        private Texture2D _texture;
+        private Vector2 _position;
+        private int _size;
+        private Color _color;
+
+        public ctverec(GraphicsDevice graphicsDevice, Vector2 position, int size, Color color)
+        {
+            _position = position;
+            _size = size;
+            _color = color;
+
+
+            _texture = new Texture2D(graphicsDevice, 1, 1);
+            _texture.SetData(new[] { Color.Red });
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, new Rectangle((int)_position.X, (int)_position.Y, _size, _size), _color);
+        }
+        public void SetPosition(Vector2 newPos)
+        {
+            _position = newPos;
+        }
+
+        public Vector2 GetPosition()
+        {
+            return _position;
+        }
+        
+    }
+    
+}
